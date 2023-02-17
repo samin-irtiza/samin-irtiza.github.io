4b6bcca79f8ebce9146e9fe4b921277aea78b283
samin-irtiza <62540152+samin-irtiza@users.noreply.github.com>
2023-02-17 18:44:52 +0600

    centered profilePhoto and text

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
-import './App.css';
-import { Card,Container,Box,Paper,Typography } from '@mui/material';
-import CssBaseline from '@mui/material/CssBaseline';
-import {PagePaper} from "./components/PagePaper";
-import { color } from '@mui/system';
+import { Card, Container, Box, Paper, Typography } from "@mui/material";
+import { PagePaper } from "./components/PagePaper";
+import { color } from "@mui/system";
+import { useTheme } from "@mui/material/styles";
+import profilePhoto from "./images/profilePhoto.png";
 function App() {
-
+  const theme = useTheme();
+  console.log(theme);
   return (
     <div className="App">
-      <CssBaseline />
+      <Container>
+        <Paper
+          sx={{
+            backgroundColor: "primary.main",
+          }}
+        >
+          <div style={{ display: "grid", justifyItems: "center" }}>
+            <div
+              className="profilePhoto"
+              style={{
+                display: "flex",
+                border: "2px solid white",
+                borderRadius: "50%",
+                height: "100px",
+                width: "100px",
+                position: "relative",
+                overflow: "hidden",
+              }}
+            >
+              <img
+                src={profilePhoto}
+                alt="author"
+                style={{
+                  alignContent: "center",
+                  margin: "0 auto",
+                  height: "125%",
+                  width: "auto",
+                }}
+              />
+            </div>
+            <Typography variant="h4">Md. Samin Irtiza</Typography>
+          </div>
+          fsdfsdfd
+        </Paper>
+      </Container>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { createTheme,ThemeProvider } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const theme=createTheme({
@@ -21,6 +22,7 @@ const theme=createTheme({
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>  
+    <CssBaseline />
       <App />
     </ThemeProvider>
   </React.StrictMode>
@@ -29,4 +31,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
